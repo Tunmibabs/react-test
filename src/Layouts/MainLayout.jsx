de26280b40ee6661ea04734560dc68c12c069d8a
codedhola <codedhola2000@gmail.com>
2024-07-28 22:11:31 +0100

    fix: fix error boundary

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,13 +3,16 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import { ErrorBoundary } from 'react-error-boundary';
 
 const MainLayout = () => {
   return (
     <>
+     <ErrorBoundary fallback={<h1>Error occured, please try again</h1>} >
         <Navbar />
         <Outlet />
         <ToastContainer />
+     </ErrorBoundary>
     </>
   )
 }
